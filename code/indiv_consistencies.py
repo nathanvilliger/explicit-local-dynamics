d92a242cb8fdd7da100238332d8ad497a4b9e7d8
Nathan Villiger <nvillig2@uoregon.edu>
2023-02-24 10:27:21 -0800

    revisions

--- code/indiv_consistencies.py
+++ code/indiv_consistencies.py
@@ -16,6 +16,8 @@ import numpy as np
 import pandas as pd
 import glob
 from scipy.optimize import curve_fit
+from scipy.stats import linregress
+import os
 
 def power_fit(x, a, b):
     return a * x**b
@@ -25,7 +27,8 @@ def infer_mus(percentages, mus=[2.5, 2.0, 1.5], max_nums=[10, 100]):
     Infer and record kernel exponent for every single growth curve I have available at each
     combination of parameters.
     '''
-    cols = ['max_num', 'mu', 'percent_local', 'inferred']
+    cols = ['max_num', 'mu', 'percent_local', 'filename', 'inferred', 'R2', 'lm_inferred', 'lm_R2',
+            'max generation', 'number of x values in the fit']
     results = pd.DataFrame(columns=cols)
     Nbad = 0
 
@@ -36,6 +39,14 @@ def infer_mus(percentages, mus=[2.5, 2.0, 1.5], max_nums=[10, 100]):
             for mu in mus:
                 d = d0 + f'mu{mu}_{p}local/'
 
+                # or longer sims?
+                ref_version = f'consistency_{max_num}_longer/mu{mu}_{p}local/'
+                if os.path.exists(ref_version):
+                    d = ref_version
+                    cutoff_threshold = 15e6
+                else:
+                    cutoff_threshold = 5e5
+
                 pop_datas = glob.glob(d + 'pop_data*.txt')
                 for f in pop_datas:
                     # pop_data = pd.read_csv(f)
@@ -43,13 +54,15 @@ def infer_mus(percentages, mus=[2.5, 2.0, 1.5], max_nums=[10, 100]):
                         pop_data = pd.read_csv(f)
                         # print('good')
                     except pd.errors.EmptyDataError:
-                        print('bad -', f)
+                        # print('bad -', f)
                         Nbad += 1
                         continue
 
-                    if np.max(pop_data.num_individuals) < 5e5: continue # sim got cut off
+                    if np.max(pop_data.num_individuals) < cutoff_threshold: continue # sim got cut off
                     times = np.array(pop_data.generation)
                     sizes = np.array(pop_data.ell)
+                    # sizes = np.sqrt(np.array(pop_data.num_individuals) / np.pi)
+                    # if len(times) == 1: continue
 
                     even_gens = (times % 2 == 0)
                     later = (times >= times.max()/2)
@@ -64,54 +77,29 @@ def infer_mus(percentages, mus=[2.5, 2.0, 1.5], max_nums=[10, 100]):
                     fit_pars = curve_fit(f=power_fit, xdata=xvals, ydata=yvals, maxfev=400000)[0]
                     inferred = fit_pars[1] - 2 # because we fit for mu + d
 
-                    df = pd.DataFrame(data=[[max_num, mu, p, inferred]], columns=cols)
+                    ypred = power_fit(xvals, *fit_pars)
+                    SS_res = np.sum((yvals - ypred)**2)
+                    SS_tot = np.sum((yvals - np.mean(yvals))**2)
+                    R2 = 1 - SS_res / SS_tot
+
+                    lm = linregress(np.log(xvals), np.log(yvals))
+                    lm_inferred = lm.slope - 2 # because we fit for mu + d
+                    lm_R2 = lm.rvalue**2
+
+                    df = pd.DataFrame(data=[[max_num, mu, p, f, inferred, R2, lm_inferred, lm_R2,
+                                             np.max(times), len(xvals)]],
+                                      columns=cols)
                     results = results.append(df)
 
     results.sort_values(by=['max_num', 'mu', 'percent_local'], inplace=True)
     results.reset_index(drop=True, inplace=True)
-    print(f'The number of empty pop_data files was {Nbad}')
+    # print(f'The number of empty pop_data files was {Nbad}')
     return results
 
-def boots(x, n_sets=10000, col_name='inferred'):
-    '''
-    Perform bootstrap resampling of data. Do n_sets of n_samples samples with replacement
-    from my data set, where n_samples is the same as the number of samples in x.
-    Return mean and standard deviation of set means.
-    To be used as DataFrame.groupby().apply(boots) with appropriately specified keys within groupby().
-
-    **x is a data frame of group values.** x is automatically fed in when function used as above.
-    '''
-    n_samples = len(x[col_name])
-    samps = np.random.choice(x[col_name], size=(n_samples, n_sets), replace=True)
-    boot_means = np.mean(samps, axis=0)
-    x['boot_mean'] = np.mean(boot_means)
-    x['boot_se'] = np.std(boot_means)
-
-    # end positions of 95% confidence interval for error bars
-    x['boot_lower'] = np.percentile(boot_means, 2.5)
-    x['boot_upper'] = np.percentile(boot_means, 97.5)
-
-    return x
-
+# ps = sorted([1] + list(range(0, 81, 5)) + [82, 85, 88] + list(range(90, 100)) + [98.5, 99.5, 99.7])
 ps = list(range(0, 96, 5)) + [99, 99.5, 99.7]
 max_nums = [10, 100]
 
 # do the individual inferences
 df = infer_mus(ps, max_nums=max_nums)
-df.to_csv('indiv_consistencies.csv', index=False)
-
-# now do the bootstrap resampling
-indiv_boots = df.groupby(['max_num', 'mu', 'percent_local']).apply(boots)
-
-# and collapse to simple data frame with one entry per parameter group.
-# compute mean and std of indiv inferences.
-columns = list(indiv_boots.columns)
-columns.remove('inferred')
-boot_means = indiv_boots.groupby(columns).aggregate(['count', 'mean', 'median', 'std'])['inferred'].reset_index()
-boot_means['SEM'] = boot_means['std'] / np.sqrt(boot_means['count'])
-
-# compute length of lower and upper portions of error bars from mean of individual inferences
-boot_means['low_ebar'] = np.abs(boot_means['mean'] - boot_means['boot_lower'])
-boot_means['high_ebar'] = np.abs(boot_means['mean'] - boot_means['boot_upper'])
-
-boot_means.to_csv('boot_consistencies.csv', index=False)
+df.to_csv('indiv_consistencies_w_lm_and_longer.csv', index=False)
